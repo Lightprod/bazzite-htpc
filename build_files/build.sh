56ad894163521c7f01cc63ee5cd40764f5a939c5
Lightprod <Lightprod@outlook.fr>
2025-07-12 12:42:50 +0200

    Ajout de Plasma-browser-integration, konsole, heroic game launcher

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y copr enable atim/heroic-games-launcher
+dnf5 install -y tmux plasma-browser-integration konsole heroic-games-launcher-bin
+dnf5 -y copr disable atim/heroic-games-launcher
 
 # Use a COPR Example:
 #
