11ad56d81ab627ae7f49b8c2d1391f2102839da5
Lightprod <Lightprod@outlook.fr>
2025-07-19 18:53:47 +0200

    Modifications des programmes

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,87 @@
 
 set -ouex pipefail
 
+# Define variables
+
+echo "Defining variables"
+
+    # COPR Repos
+COPR_REPOS=(
+    atim/heroic-games-launcher
+)
+    # Layered packages
+LAYERED_REPO_PACKAGES=(
+    konsole
+    heroic-games-launcher-bin
+    cockpit 
+    cockpit-files 
+    cockpit-ostree 
+    gvfs-smb 
+    gvfs-fuse 
+    plasma-discover 
+    plasma-discover-flatpak 
+    plasma-discover-notifier
+    plasma-discover-kns
+    kvantum
+    mc
+    atuin
+    openrgb
+    gparted
+)
+
+LAYERED_DISTANT_PACKAGES=( # For packages not present in Fedora's repos
+    https://launchpad.net/veracrypt/trunk/1.26.24/+download/veracrypt-1.26.24-Fedora-40-x86_64.rpm
+)
+
+    # Flatpaks
+FLATPAK_ADD=(
+    one.ablaze.floorp
+    io.github.ungoogled_software.ungoogled_chromium
+    com.bitwarden.desktop
+    io.github.ilya_zlobintsev.LACT
+    io.github.dvlv.boxbuddyrs
+    net.davidotek.pupgui2
+    com.github.zocker_160.SyncThingy
+    tv.plex.PlexDesktop
+    tv.plex.PlexHTPC
+    com.plexamp.Plexamp
+    com.github.iwalton3.jellyfin-media-player
+    com.discordapp.Discord
+    dev.vencord.Vesktop
+)
+
+FLATPAK_REMOVE=(
+    org.mozilla.firefox
+)
+
+
+echo "Enabling COPR repos"
+
+    for repo in "${COPR_REPOS[@]}"; do
+        dnf5 copr enable "$repo" -y
+    done
+
+echo "Installing packages"
+
+    dnf5 install --skip-broken -y "${LAYERED_REPO_PACKAGES[@]}"
+    dnf5 install --skip-broken -y "${LAYERED_DISTANT_PACKAGES[@]}"
+
+echo "Disabling COPR repos"
+    for repo in "${COPR_REPOS[@]}"; do
+        dnf5 copr disable "$repo" -y
+    done
+
+echo "Installing additional flatpaks"
+    flatpak install -y -v --system "${FLATPAK_ADD[@]}"
+
+echo "Removing un-needed flatpaks"
+    flatpak install -y -v "${FLATPAK_REMOVE[@]}"
+
+echo "Enabling system unit files"
+    systemctl enable cockpit.socket
+    systemctl enable sshd # For SSH access
+
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,9 +91,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 -y copr enable atim/heroic-games-launcher
-dnf5 install -y tmux plasma-browser-integration konsole heroic-games-launcher-bin
-dnf5 -y copr disable atim/heroic-games-launcher
+# dnf5 -y copr enable atim/heroic-games-launcher
+# dnf5 install -y konsole heroic-games-launcher-bin cockpit cockpit-files cockpit-ostree gvfs-smb gvfs-fuse plasma-discover plasma-discover-flatpak plasma-discover-notifier 
+# dnf5 -y copr disable atim/heroic-games-launcher
 
 # Use a COPR Example:
 #
@@ -23,4 +104,4 @@ dnf5 -y copr disable atim/heroic-games-launcher
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
