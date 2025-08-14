4e49e56633699d3e588bae5ea2ce4fa0a75b3287
Lightprod <Lightprod@outlook.fr>
2025-08-14 19:47:17 +0200

    Rewrite file change script

---
+++ build_files/steps/04-file_change.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+echo "Defining variables"
+
+DISTROSELF_DIR=/etc/skel/.var/app/com.ranfdev.DistroShelf
+
+UJUST_WORK_FOLDER=/tpm/ujust
+UJUST_CONFIG_FILE=/usr/share/ublue-os/justfile
+
+SYSTEMD_USER_SERVICE_FOLDER=/etc/skel/.config/systemd/user/*
+SYSTEMD_USER_SERVICE_SYSLINK_FOLDER=/etc/skel/.config/systemd/user/default.target.wants
+
+GIT_UJUST_CONFIG_FILE=/ctx/files/justfile
+
+# ===================================================================
+
+mkdir ${UJUST_WORK_FOLDER}
+
+# ===================================================================
+
+# Remove default DistroShelf config
+
+echo 'Removing distroshelf config'
+
+rm -rd $DISTROSELF_DIR
+
+# ===================================================================
+
+echo 'Copying upstream justfile'
+
+cd ${UJUST_WORK_FOLDER}
+cp $UJUST_CONFIG_FILE upstream-justfile
+cp $GIT_UJUST_CONFIG_FILE justfile-git
+
+echo 'Amend upstream justfile' # Add customs just recipes
+
+cat justfile-git >> upstream-justfile
+cat upstream-justfile
+
+echo 'Copying justfile to usr'
+
+cp upstream-justfile $JUST_CONFIG_FILE
+
+# ===================================================================
+
+# mkdir ${SYSTEMD_USER_SERVICE_SYSLINK_FOLDER}
+# cp $SYSTEMD_USER_SERVICE_FOLDER $SYSTEMD_USER_SERVICE_SYSLINK_FOLDER
