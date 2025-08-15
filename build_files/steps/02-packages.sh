655f84d747c2d96cef52659eb908fe1cf6d5ef3d
Lightprod <Lightprod@outlook.fr>
2025-08-15 10:21:13 +0200

    Fix packages workflow

--- build_files/steps/02-packages.sh
+++ build_files/steps/02-packages.sh
@@ -16,10 +16,7 @@ GIT_RPM_INSTALL_LIST_FILE=/ctx/files/dnf/packages_install
 GIT_DISTANT_RPM_INSTALL_LIST_FILE=/ctx/files/dnf/non-repo_packages
 GIT_RPM_UNINSTALL_LIST_FILE=/ctx/files/dnf/packages_remove
 
-COPR_REPOS=""
-PACKAGE_REMOVE=""
-REPO_PACKAGES_INSTALL=""
-DISTANT_PACKAGES_INSTALL=""
+
 
 # ================================================================================
 
@@ -34,29 +31,32 @@ cp $GIT_RPM_UNINSTALL_LIST_FILE $RPM_UNINSTALL_LIST_FILE
 
 # ================================================================================
 
-# echo "Setting dnf variables"
+echo "Setting dnf variables"
 
-# COPR_REPOS=$COPR_REPOS_FILE
+COPR_REPOS=$(< $COPR_REPOS_FILE)
+PACKAGE_REMOVE=$(< $RPM_UNINSTALL_LIST_FILE)
+REPO_PACKAGES_INSTALL=$(< $RPM_INSTALL_LIST_FILE)
+DISTANT_PACKAGES_INSTALL=$(< $DISTANT_RPM_INSTALL_LIST_FILE)
 
 # ================================================================================
 
 echo "Enabling COPR repos"
 
-    # for repo in "${COPR_REPOS[@]}"; do
-        dnf5 copr enable $COPR_REPOS_FILE -y
-    # done
+    for repo in $COPR_REPOS; do
+        dnf5 copr enable $repo -y
+    done
 
 echo "Removing packages"
 
-    dnf5 remove -y $RPM_UNINSTALL_LIST_FILE
+    dnf5 remove -y $RPM_UNINSTALL_LIST
 
 echo "Installing packages"
 
-    dnf5 install --skip-broken --skip-unavailable -y $RPM_INSTALL_LIST_FILE
-    dnf5 install --skip-broken --skip-unavailable -y $DISTANT_RPM_INSTALL_LIST_FILE
+    dnf5 install --skip-broken --skip-unavailable -y $RPM_INSTALL_LIST
+    dnf5 install --skip-broken --skip-unavailable -y $DISTANT_RPM_INSTALL_LIST
 
 echo "Disabling COPR repos"
 
-    # for repo in "${COPR_REPOS[@]}"; do
-        dnf5 copr disable $COPR_REPOS_FILE -y
-    # done
+    for repo in $COPR_REPOS; do
+        dnf5 copr disable $COPR_REPOS -y
+    done
