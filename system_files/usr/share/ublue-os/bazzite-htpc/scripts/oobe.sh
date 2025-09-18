c75acc71787d9d3cee78183ae608786c1494470d
Lightprod <Lightprod@outlook.fr>
2025-09-18 19:03:03 +0200

    Fix oobe service not working

--- system_files/usr/share/ublue-os/bazzite-htpc/scripts/oobe.sh
+++ system_files/usr/share/ublue-os/bazzite-htpc/scripts/oobe.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/bash
 
 echo 'Installing flatpaks...'
-ujust setup-flatpak-repo
-ujust install-system-flatpaks
 
+ujust install-system-flatpaks
 
 echo 'Enabling default flatpak service'
 
 systemctl enable enable default-flatpak-manager
+
+echo "Disabling oobe service"
+
+systemctl disable systemctl enable oobe
