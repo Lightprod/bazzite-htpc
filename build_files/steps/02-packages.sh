ff88bdc5ac13d42493de0244e1ceb93c620ae5b5
Lightprod <Lightprod@outlook.fr>
2025-09-23 20:24:12 +0200

    Move COPR packages from main install command

--- build_files/steps/02-packages.sh
+++ build_files/steps/02-packages.sh
@@ -8,11 +8,13 @@ DEFAULT_FOLDER=/tmp/dnf
 
 COPR_REPOS_FILE=/tmp/dnf/copr_repos
 RPM_INSTALL_LIST_FILE=/tmp/dnf/repo_rpm_install
+COPR_RPM_INSTALL_LIST_FILE=/tmp/dnf/copr_rpm_install
 DISTANT_RPM_INSTALL_LIST_FILE=/tmp/dnf/distant_rpm_install
 RPM_UNINSTALL_LIST_FILE=/tmp/dnf/rpm_uninstall
 
 GIT_COPR_REPOS_FILE=/ctx/files/dnf/copr_repos
 GIT_RPM_INSTALL_LIST_FILE=/ctx/files/dnf/packages_install
+GIT_COPR_RPM_INSTALL_LIST_FILE=/ctx/files/dnf/copr_packages_install
 GIT_DISTANT_RPM_INSTALL_LIST_FILE=/ctx/files/dnf/non-repo_packages
 GIT_RPM_UNINSTALL_LIST_FILE=/ctx/files/dnf/packages_remove
 
@@ -25,6 +27,7 @@ echo 'Copying gitfiles to tmp'
 mkdir ${DEFAULT_FOLDER}
 
 cp $GIT_COPR_REPOS_FILE $COPR_REPOS_FILE
+cp $GIT_COPR_RPM_INSTALL_LIST_FILE $COPR_RPM_INSTALL_LIST_FILE
 cp $GIT_RPM_INSTALL_LIST_FILE $RPM_INSTALL_LIST_FILE
 cp $GIT_DISTANT_RPM_INSTALL_LIST_FILE $DISTANT_RPM_INSTALL_LIST_FILE
 cp $GIT_RPM_UNINSTALL_LIST_FILE $RPM_UNINSTALL_LIST_FILE
@@ -36,9 +39,23 @@ echo "Setting dnf variables"
 COPR_REPOS=$(< $COPR_REPOS_FILE)
 PACKAGE_REMOVE=$(< $RPM_UNINSTALL_LIST_FILE)
 REPO_PACKAGES_INSTALL=$(< $RPM_INSTALL_LIST_FILE)
+COPR_PACKAGES_INSTALL=$(< $COPR_RPM_INSTALL_LIST_FILE)
 DISTANT_PACKAGES_INSTALL=$(< $DISTANT_RPM_INSTALL_LIST_FILE)
 
 # ================================================================================
+
+echo "Removing packages"
+
+    dnf5 remove -y $PACKAGE_REMOVE
+
+echo "Installing repos packages"
+
+    dnf5 install --skip-broken --skip-unavailable -y $REPO_PACKAGES_INSTALL
+
+echo "Installing distant packages"
+
+    dnf5 install --skip-broken --skip-unavailable -y $DISTANT_PACKAGES_INSTALL
+
 
 echo "Enabling COPR repos"
 
@@ -46,14 +63,10 @@ echo "Enabling COPR repos"
         dnf5 copr enable $repo -y
     done
 
-echo "Removing packages"
+echo "Installing copr packages"
 
-    dnf5 remove -y $PACKAGE_REMOVE
+    dnf5 install --skip-broken --skip-unavailable -y $COPR_PACKAGES_INSTALL
 
-echo "Installing packages"
-
-    dnf5 install --skip-broken --skip-unavailable -y $REPO_PACKAGES_INSTALL
-    dnf5 install --skip-broken --skip-unavailable -y $DISTANT_PACKAGES_INSTALL
 
 echo "Disabling COPR repos"
 
